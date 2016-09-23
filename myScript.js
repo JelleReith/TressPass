f16b1ae12083737f48c14d6582a6e88bfcccad28
JelleReith <jelle.reith@hotmail.com>
2016-09-23 14:25:12 +0200

    Update

--- myScript.js
+++ myScript.js
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-
-  d3.csv("data/ATM Attack Events-Table 1.csv", function(data){
-    });
-=======
 var myData = [];
 
 d3.csv("data/ATM Attack Events-Table 1.csv", function(error, data) {
   if (error) throw error;
-  // console.log(data); // [{"Hello": "world"}, …]
-
   for (var i = 0; i < data.length; i++) {
-    // console.log(data[i]);
     myData.push(data[i]);
-
   }
 });
->>>>>>> origin/master
