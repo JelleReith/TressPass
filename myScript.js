caa8fb07aed85e4283c14b4304cf459f2ae0d1f0
ipibo <iboibelings@gmail.com>
2016-09-23 14:23:32 +0200

    a

--- myScript.js
+++ myScript.js
@@ -1,3 +1,12 @@
-d3.csv("data/ATM Attack Events-Table 1.csv", function(data) {
-  console.log(data[0]);
+var myData = [];
+
+d3.csv("data/ATM Attack Events-Table 1.csv", function(error, data) {
+  if (error) throw error;
+  // console.log(data); // [{"Hello": "world"}, …]
+
+  for (var i = 0; i < data.length; i++) {
+    // console.log(data[i]);
+    myData.push(data[i]);
+
+  }
 });
