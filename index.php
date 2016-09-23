804cc51b12204e71be61a1a9b4c96e10870a84d4
JelleReith <jelle.reith@hotmail.com>
2016-09-23 13:28:29 +0200

    Hoi

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-Hallo
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"> </script>
+    <script src = "https://d3js.org/d3.v4.js"> </script>
+    <script src = "myScript.js"> </script>
+  </head>
+  <body>
+
+  </body>
+</html>
---
+++ myScript.js
@@ -0,0 +1,3 @@
+d3.csv("data/ATM Attack Events-Table 1.csv", function(data) {
+  console.log(data[0]);
+});
